dc8742d04c1a5b46d7b047af2c075ad8f3055314
YCHE0392 <112544865+YCHE0392@users.noreply.github.com>
2024-05-08 21:38:16 +1000

    Update FavProductDatabase.kt

--- app/src/main/java/com/example/marketplace/FavProductDatabase.kt
+++ app/src/main/java/com/example/marketplace/FavProductDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Product::class], version = 2, exportSchema = false)
+@Database(entities = [Product::class], version = 3, exportSchema = false)
 abstract class FavProductDatabase : RoomDatabase() {
     abstract fun favProductDAO(): FavProductDAO
 
