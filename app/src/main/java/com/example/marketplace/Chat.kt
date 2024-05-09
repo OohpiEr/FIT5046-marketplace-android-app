53af6eb4a34e37af10bfac53d2914050ef6d962a
YCHE0392 <112544865+YCHE0392@users.noreply.github.com>
2024-05-09 23:27:43 +1000

    Update Chat.kt

--- app/src/main/java/com/example/marketplace/Chat.kt
+++ app/src/main/java/com/example/marketplace/Chat.kt
@@ -429,7 +429,6 @@ fun ContactScreen(viewModel: MessageViewModel, navController: NavHostController)
                         )
                 }
             },
-            scrollBehavior = scrollBehavior,
         )
     },
         bottomBar = {
